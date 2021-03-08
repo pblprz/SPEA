904276961ad7720eb3f54291659402723cb42d88
plbprz <pablopj.97@gmail.com>
2021-03-08 20:14:48 +0100

    Sesión de clase 2

--- deviceIoT.py
+++ deviceIoT.py
@@ -1,5 +1,71 @@
-#import paho-mqtt # https://python.org/pypi/paho-mqtt
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import dh
+from cryptography.hazmat.primitives.serialization import ParameterFormat
+from cryptography.hazmat.primitives.serialization import Encoding
+from cryptography.hazmat.primitives.asymmetric.dh import DHParameterNumbers
+from cryptography.hazmat.primitives.serialization import load_pem_parameters
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.ciphers import aead
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 import paho.mqtt.client as mqtt
+import os
+
+# Solo si queremos Padding
+from cryptography.hazmat.primitives import padding
+
+# Generate DH parameters
+'''
+parameters = dh.generate_parameters(generator=2, key_size=512, backend=default_backend())
+params_pem = parameters.parameter_bytes(Encoding.PEM, ParameterFormat.PKCS3)
+print(str(params_pem))
+'''
+
+print('Dame los parámetros: ')
+b_pem = bytes(input().replace("\\n", "\n"), 'ascii')
+# b_pem = codecs.decode(input(), "unicode-escape")
+print(b_pem)
+parameters = load_pem_parameters(b_pem, backend=default_backend())
+
+#b_params_from_number = DHParameterNumbers(10, 3).parameters(backend=default_backend())
+#b_params_from_pem = load_pem_parameters(b_pem, backend=default_backend())
+
+#b_params_pem= b_params.parameter(backend=default_backend())
+
+#parameters = b_params_from_number
+
+#Generate private keys.
+a_private_key = parameters.generate_private_key()
+a_public_key = a_private_key.public_key()
+
+print("Esta es mi clave privada: %d"%a_private_key.private_numbers().x)
+print("Esta es mi clave pública: %d"%a_public_key.public_numbers().y)
+
+'''
+b_private_key = parameters.generate_private_key()
+b_public_key = b_private_key.public_key()
+'''
+
+print("Dame la pública de tu compañero: ")
+b_public_key_number = int(input())
+
+#Des-serializando
+peer_public_numbers = dh.DHPublicNumbers(b_public_key_number, parameters.parameter_numbers())
+b_public_key = peer_public_numbers.public_key(default_backend())
+
+'''
+print("Esta es tu clave privada: %d"%b_private_key.private_numbers().x)
+print("Esta es tu clave pública: %d"%b_public_key.public_numbers().y)
+'''
+a_shared_key = a_private_key.exchange(b_public_key)
+#b_shared_key = b_private_key.exchange(a_public_key)
+
+print("a_shared_key: " + str(a_shared_key))
+#print("b_shared_key: " + str(b_shared_key))
+
+key = HKDF(algorithm=hashes.SHA256(), length=32, salt=None, info=b'handshake data').derive(a_shared_key)
+print(key)
 
 # 1. Iot -> S : publish "register" Pu_IoT_DH
 # 2. S -> FF :  publish "pu_S" Pu_S_DH 
--- platformIoT.py
+++ platformIoT.py
@@ -6,9 +6,12 @@ from cryptography.hazmat.primitives.asymmetric.dh import DHParameterNumbers
 from cryptography.hazmat.primitives.serialization import load_pem_parameters
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives.ciphers import aead
-from cryptography.fernet import Fernet 
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 import paho.mqtt.client as mqtt
 import os
+import base64
 
 # Solo si queremos Padding
 from cryptography.hazmat.primitives import padding
@@ -16,6 +19,7 @@ from cryptography.hazmat.primitives import padding
 # Generate DH parameters
 parameters = dh.generate_parameters(generator=2, key_size=512, backend=default_backend())
 params_pem = parameters.parameter_bytes(Encoding.PEM, ParameterFormat.PKCS3)
+print(params_pem)
 
 #b_params_from_number = DHParameterNumbers(10, 3).parameters(backend=default_backend())
 #b_params_from_pem = load_pem_parameters(b_pem, backend=default_backend())
@@ -31,17 +35,31 @@ a_public_key = a_private_key.public_key()
 print("Esta es mi clave privada: %d"%a_private_key.private_numbers().x)
 print("Esta es mi clave pública: %d"%a_public_key.public_numbers().y)
 
+'''
 b_private_key = parameters.generate_private_key()
 b_public_key = b_private_key.public_key()
+'''
 
+print("Dame la pública de tu compañero: ")
+b_public_key_number = int(input())
+
+#Des-serializando
+peer_public_numbers = dh.DHPublicNumbers(b_public_key_number, parameters.parameter_numbers())
+b_public_key = peer_public_numbers.public_key(default_backend())
+
+'''
 print("Esta es tu clave privada: %d"%b_private_key.private_numbers().x)
 print("Esta es tu clave pública: %d"%b_public_key.public_numbers().y)
-
+'''
 a_shared_key = a_private_key.exchange(b_public_key)
-b_shared_key = b_private_key.exchange(a_public_key)
+#b_shared_key = b_private_key.exchange(a_public_key)
 
 print("a_shared_key: " + str(a_shared_key))
-print("b_shared_key: " + str(b_shared_key))
+#print("b_shared_key: " + str(b_shared_key))
+
+derived_key = HKDF(algorithm=hashes.SHA256(), length=32, salt=None, info=b'handshake data').derive(a_shared_key)
+key = base64.urlsafe_b64encode(derived_key)
+print(key)
 
 #Para CBC
 iv = os.urandom(16)
@@ -54,6 +72,7 @@ backend = default_backend()
 
 ejercicio1()
 
+'''
 # Para realiza el Padding de bloques de 128 bits (AES) habría que usar un padder
 padder = padding.PKCS7(128).padder()
 
@@ -89,20 +108,21 @@ print(descifrado)
 unpadder = padding.PKCS7(128).unpadder()
 output = unpadder.update(descifrado) + unpadder.finalize()
 print(output)
+'''
 
 ## FERNET
 # key is generated 
-key = Fernet.generate_key() 
+# key = Fernet.generate_key()
   
 # value of key is assigned to a variable 
 f = Fernet(key) 
-  
+
 # the plaintext is converted to ciphertext 
 cifrado = f.encrypt(b"Hello world") 
-  
+
 # display the ciphertext 
 print(cifrado) 
-  
+
 # decrypting the ciphertext 
 descifrado = f.decrypt(cifrado) 
   
@@ -110,9 +130,14 @@ descifrado = f.decrypt(cifrado)
 # converts it from byte to string 
 print(descifrado.decode())
 
+### PRUEBA
+derived_key2 = HKDF(algorithm=hashes.SHA256(), length=24, salt=None, info=b'handshake data').derive(a_shared_key)
+key2 = base64.urlsafe_b64encode(derived_key2)
+print(key2)
+
 ## AEAD
-key = aead.AESGCM.generate_key(bit_length=128)
-aesgcm = aead.AESGCM(key)
+# key = aead.AESGCM.generate_key(bit_length=128)
+aesgcm = aead.AESGCM(key2)
 nonce = os.urandom(12)
 # aad = b"Authenticated but unencrypted data"
 cifrado = aesgcm.encrypt(nonce, b"Hello world", None)
